d2c8c20dff4b0033e9e32ecb7577ef1fff270bb4
Strangerxxx <black.danek@gmail.com>
2012-10-18 13:30:12 +0400

    Update models/admin.model.php

--- models/admin.model.php
+++ models/admin.model.php
@@ -5,23 +5,15 @@
 		global $cimsDB;
 		$password = md5($iPassword);
 		$cimsDB->start($cimsApp->getConfig("mysql_db"));
-		$request = "SELECT * FROM `users` WHERE `email` = '$iEmail' LIKE `password` = '$password'";
+		$request = "SELECT COUNT() FROM `users` WHERE `email` = '$iEmail' AND `password` = '$password'";
 		$reqResult = $cimsDB->request($request);
-		$countUsers = mysql_num_rows($reqResult);
-		while($row = mysql_fetch_array($reqResult, MYSQL_ASSOC)) {
-			switch($countUsers) {
-				case 1:
-					setcookie("auth_cache", $row['password'], time()+3600);
-					global $cimsApp;
-					global $userLogin;
-					print $row[0]['login'];
-					$cimsApp->setVar("login", $row['login']);
-					flush();
-					return true;
-					break;
-				default:
-					return false;
-			}
+		$CountUsers = mysql_fetch_row($reqResult);
+		if($CountUsers>0){
+			setcookie("auth_cache", $row['password'], time()+3600);
+			global $cimsApp;
+			global $userLogin;
+			$cimsApp->setVar("login", $row['login']);
+			return true;
 		}
 	}
 	
